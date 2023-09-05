5c24c12d9fe6ee3a0e8dba6e3ec1698d6d8438a2
Sanaz Rahbari <61431041+SanazR@users.noreply.github.com>
2023-09-05 22:52:37 +0400

    Add files via upload

---
+++ Basics/Data_Types_Basics/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /*Console.Write("Hello World! ");
+      Console.Write("May name is Sani. ");  
+      Console.Write(5+8); */
+
+      /* int myNum = 63;
+      double myDoubleNum = 2.35666661D;
+      float myfloatnum = 2.502F
+      int numLong = 120000500L; //Long int
+      char myLetter = 'D'; //The character must be surrounded by single quotes
+      bool myBool = true;
+      string myText = "Hello"; //String values must be surrounded by double quotes
+
+      ---------------------------------------------------------
+      The precision of float is only six or seven decimal digits, 
+      while double variables have a precision of about 15 digits. 
+      Therefore it is safer to use double for most calculations.
+      ----------------------------------------------------------
+
+      Console.WriteLine(myNum); */
+      
+      /* string fname="Sara";
+      string lanme= " Johnson";
+      string fullname= fname+lanme;
+
+      Console.WriteLine(fullname); */
+
+      float f1 = 35e3F;
+      double d1 = 12E4D;
+
+      Console.WriteLine(f1);
+      Console.WriteLine(d1);
+
+      /* int x, y, z;
+      x=y=z=10;
+      Console.WriteLine(x+y+z); */
+
+      int num1 = 9;
+      double myDouble = num1; // Automatic casting: int to double
+
+      Console.WriteLine(num1);      
+      Console.WriteLine(myDouble);
+
+      double numD = 9.78;
+      int num2 = (int) numD; // Manual casting: double to int
+
+      Console.WriteLine(numD);      
+      Console.WriteLine(num2);
+
+    }
+  }
+}
---
+++ Basics/Function/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void MyMethod(string fname="Hana", int age=10)
+    {
+      Console.WriteLine(fname + " Johnson is " + age);
+    }
+
+    static int MySum(int x, int y){
+        return x+y;
+    }
+
+    static void KeyName(string key1, string key2, string key3){
+        Console.WriteLine("The newest key is " + key3);
+    }
+
+    static int PlusMethodInt(int x, int y)
+    {
+      return x + y;
+    }
+
+    static double PlusMethodDouble(double x, double y)
+    {
+      return x + y;
+    }
+
+    static void Main(string[] args)
+    {
+      //MyMethod("Sara", 22);
+      //MyMethod("Daniel", 16);
+      MyMethod(); //Default value
+
+      int z = MySum(8,16);
+      Console.WriteLine (MySum(5,2));
+      Console.WriteLine (z);
+
+      KeyName(key2:"abc1", key3:"def2", key1:"ghi3"); // In this wayt thorder of the arg doesn't matter
+      KeyName("jkl4","mno5","pqr6");
+      
+      int myNum1 = PlusMethodInt(8, 5);
+      double myNum2 = PlusMethodDouble(4.3, 6.26);
+      Console.WriteLine("Int: " + myNum1);
+      Console.WriteLine("Double: " + myNum2);
+      
+    }
+  }
+}

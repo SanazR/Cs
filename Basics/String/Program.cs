70f16b4488923564eddcd54bf1c333a13f9a2401
Sanaz Rahbari <61431041+SanazR@users.noreply.github.com>
2023-09-05 23:00:20 +0400

    Add files via upload

---
+++ Basics/String/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /* string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      string txt1 = "abcdefghijklmnopqrstuvwxyz";
+
+      Console.WriteLine("The length of the txt is: " + txt.Length );
+      Console.WriteLine(txt1.ToUpper());
+      Console.WriteLine(txt.ToLower()); */
+
+      // Anotherway forconcatinating strings (instead of "+") 
+      string firstName = "John ";
+      string lastName = "Doe";
+      string name = string.Concat(firstName, lastName);
+      Console.WriteLine(name);
+
+      string name1 = $"My name is: {firstName}{lastName}";
+      Console.WriteLine(name1);
+
+      Console.WriteLine(name[1]);
+
+
+
+      // Full name
+      string name2 = "John Doe";
+
+      // Location of the letter D
+      int charPos = name2.IndexOf("D");
+
+      // Get last name
+      string lName = name2.Substring(charPos);
+
+      // Print the result
+      Console.WriteLine(lName);
+
+      string txt2 = "We are the so-called \"Vikings\" from the north.";
+      Console.WriteLine(txt2);
+
+      string txt3= "It\'s a cat.";
+      Console.WriteLine(txt3);
+      
+    }
+  }
+}

65f8dbbc06bb1caabd1bdd791b018d37b992a5c6
Sanaz Rahbari <61431041+SanazR@users.noreply.github.com>
2023-09-05 22:50:22 +0400

    Add files via upload

---
+++ Basics/Conditions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int time=22;
+      string result =(time<18) ? "Good day!" : "Good night!";
+      Console.WriteLine(result);
+
+
+      // Switch
+      int day=4;
+      switch (day){
+        case 1:
+          Console.WriteLine("Monday");
+          break;
+        case 2:
+          Console.WriteLine("Tuesday");
+          break;
+        case 3:
+          Console.WriteLine("Wednesday");
+          break;
+        case 4:
+          Console.WriteLine("Thursday");
+          break;
+        case 5:
+          Console.WriteLine("Friday");
+          break;
+        default:
+          Console.WriteLine("Looking forward to the Weekend.");
+          break;
+      }
+    }
+  }
+}
---
+++ Basics/Constructors/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApplication
+{
+  // Create a Car class
+  class Car
+  {
+    public string model;
+    public string color;
+    public int year;
+
+    // Create a class constructor for the Car class
+    /*public Car()
+    {
+      model = "Mustang"; // Set the initial value for model
+    } */
+    public Car(string modelName, string colorName, int modelYear){
+        model = modelName;
+        color = colorName;
+        year = modelYear;
+    }
+
+    static void Main(string[] args)
+    {
+      // Car Ford = new Car();  // Create an object of the Car Class (this will call the constructor)
+      Car Ford = new Car ("Mustang","blue", 1994);
+      Car Opel = new Car("Astra", "White", 2005);
+      Console.WriteLine(Ford.model + " " + Ford.year + " " + Ford.color); 
+      Console.WriteLine(Opel.model + " " + Opel.year + " " + Opel.color); 
+    }
+  }
+}

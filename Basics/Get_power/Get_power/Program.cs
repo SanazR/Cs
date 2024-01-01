e17175118a4b20799cf96ecd72cf7d0f79d895b1
Sanaz Rahbari <61431041+SanazR@users.noreply.github.com>
2024-01-01 22:07:07 +0400

    Add files via upload

---
+++ Basics/Get_power/Get_power/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace myApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( GetPow (3, 2));
+            Console.ReadLine();
+        }
+
+        static int GetPow(int baseNum, int pow)
+        {
+            int result = 1;
+
+            for (int i = 0; i<pow; i++)
+            {
+                result *= baseNum;
+            }
+
+            return result;
+        }
+    }
+}
+
+
+
+

1c986c07023fd763ab5765b6b3d0ee9503c1796a
Sanaz Rahbari <61431041+SanazR@users.noreply.github.com>
2024-01-01 21:42:49 +0400

    Add files via upload

---
+++ Gussing_Game/Gussing_Game/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+  
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "snow";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses= false;
+
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.WriteLine("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+                
+            }
+
+            if (outOfGuesses)
+            {
+                Console.WriteLine("You Lose!");
+            }
+            else
+            {
+                Console.WriteLine("You Win!");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
+
+
+

c0e08442996b9778aad2719162ea4dacaf212a81
Sanaz Rahbari <61431041+SanazR@users.noreply.github.com>
2024-01-07 23:03:43 +0400

    Add files via upload

---
+++ Basics/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int Num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter a number: ");
+                int Num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(Num1 / Num2);
+            }
+            
+            /*
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }  
+            */
+
+            catch (DivideByZeroException ex) {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+

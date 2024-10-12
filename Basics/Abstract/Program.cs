8fd9478fc9066ccd8239b0087bb520741e1526ce
Sanaz Rahbari <61431041+SanazR@users.noreply.github.com>
2024-10-12 20:34:25 +0400

    Add files via upload

---
+++ Basics/Abstract/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyApp{
+    abstract class Animal{
+        public abstract void animalSound(); //anstract method doesn't have a body
+        public void sleep(){  //Regular method
+            Console.WriteLine("Zzz");
+        } 
+
+    }
+
+    class Pig: Animal{
+        public override void animalSound(){ //The body for animalSound() has been provided here
+            Console.WriteLine("The pig says Oinks Oinks.");
+        }
+    }
+
+    class Program{
+        static void Main(string[] args){
+            Pig myPig = new Pig();
+          
+            myPig.animalSound();
+            myPig.sleep();
+        }
+    }
+
+}
---
+++ Basics/Polymorphism/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp{
+
+    class Animal{
+        public virtual void animalSound(){
+            Console.WriteLine("The animal makes a sound.");
+        }
+    }
+
+    class Pig: Animal{
+        public override void animalSound(){
+            Console.WriteLine("The pig says Oinks Oinks.");
+        }
+    }
+
+    class Cat:Animal{
+        public override void animalSound(){
+            Console.WriteLine("The cat says Meow Meow.");
+        }
+    }
+
+    class Program{
+        static void Main(string[] args){
+            Animal myAnimal = new Animal(); // Create an animal obj
+            Animal myPig = new Pig();
+            Animal myCat = new Cat();
+
+            myAnimal.animalSound();
+            myPig.animalSound();
+            myCat.animalSound();
+        }
+    }
+}
+
+

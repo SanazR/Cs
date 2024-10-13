27d312aae0c3e3d2954807b5763bb0a3b88dd5d2
Sanaz Rahbari <61431041+SanazR@users.noreply.github.com>
2024-10-13 21:09:33 +0400

    Add files via upload

---
+++ Basics/Multi_Interface/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp2{
+   interface IAnimal{
+
+    //inteface methods doesn't have body
+     void sound(); 
+   }
+
+   interface IVehicle{
+    void run();
+   }
+
+   class Robot: IAnimal, IVehicle { //Using 2 interfaces for a child class
+
+        //The body for mehtods have been provided here
+        public void sound(){ 
+            Console.WriteLine("The robot says Oinks Oinks.");
+        }
+
+        public void run(){
+            Console.WriteLine("The rbot runs fast.");
+        }
+
+    }
+
+    class Program{
+        static void Main(string[] args) {
+            Robot myRobot = new Robot();
+            myRobot.sound();
+            myRobot.run();
+        }
+    }
+
+
+}
